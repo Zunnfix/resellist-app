1c3e476df02fca09565d3fc0e1a77f9746d0f0f7
Chase Shields <cwshields2@gmail.com>
2019-09-03 20:13:32 -0500

    added menu icon, search & links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import '../assets/sass/components/Lander.scss';
 import '../assets/sass/components/Navbar.scss'
+import '../assets/fonts/micons/micons.css';
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/logo-offerup.png';
+import menu from '../assets/images/menu-icon.png';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 export default function Navbar() {
   return (
     <nav>
-      <div className="nav-container"></div>
+      <div className="nav-container">
+        <Link to='/'><img className="logo" src={logo} alt="Logo" /></Link>
+        <Form>
+          <FormControl type="text" placeholder="Search" />
+          <Button className="btn-lte-green">GO</Button>
+        </Form>
+        <div className="link-wrap">
+          <Link className="img-link" to=''>
+            <div className="item-feature__icon">
+              <i className="icon-camera"></i>
+            </div>
+            Sell
+          </Link>
+          <Link to=''>About</Link>
+          <Link to=''>Login</Link>
+          <Link to=''>Sign up</Link>
+        </div>
+        <div className="menu-icon"><img src={menu} alt="Menu"/></div>
+      </div>
     </nav>
   );
 }
