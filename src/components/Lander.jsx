12384c8e115dee666f15902af7deeff68a9a9a61
Chase Shields <cwshields2@gmail.com>
2019-09-03 20:13:58 -0500

    inserted card component

--- src/components/Lander.jsx
+++ src/components/Lander.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import '../assets/sass/components/Lander.scss';
 import Navbar from './Navbar'
-
+import ProductCard from './ProductCard';
 
 export default class Lander extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <header></header>
+        <div className="container">
+          <div className="product-wrap">
+            <ProductCard />
+          </div>
+        </div>
       </div>
     );
   }
