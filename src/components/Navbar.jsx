dfe6b8efb035954785d2e22235c10b35bb24398d
Chase Shields <cwshields2@gmail.com>
2019-09-11 17:42:55 -0500

    fixes and dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import '../assets/sass/components/Lander.scss';
+import React from 'react'
+import '../assets/sass/components/Lander.scss'
 import '../assets/sass/components/Navbar.scss'
-import '../assets/fonts/micons/micons.css';
+import '../assets/fonts/micons/micons.css'
 import { Link } from 'react-router-dom'
-import logo from '../assets/images/Sellist-Logo.png';
-import icon from '../assets/images/Sellist-Icon.png';
-import menu from '../assets/images/menu-icon.png';
-import { Form, FormControl, Button, DropdownButton, Dropdown } from 'react-bootstrap';
-import PostModal from './PostModal';
-import LoginModal from './LoginModal';
-import RegisterModal from './RegisterModal';
+import logo from '../assets/images/Sellist-Logo.png'
+import icon from '../assets/images/Sellist-Icon.png'
+import { Form, FormControl, Button, DropdownButton, Dropdown } from 'react-bootstrap'
+import PostModal from './PostModal'
+import LoginModal from './LoginModal'
+import RegisterModal from './RegisterModal'
+import { connect } from 'react-redux'
+import Axios from 'axios'
+import { resetState } from '../redux/reducers/userReducer';
 
-export default function Navbar(props) {
-  const [registerModalShow, setRegisterModalShow] = React.useState(false);
-  const [loginModalShow, setLoginModalShow] = React.useState(false);
-  const [postModalShow, setPostModalShow] = React.useState(false);
+function Navbar(props) {
+  const [registerModalShow, setRegisterModalShow] = React.useState(false)
+  const [loginModalShow, setLoginModalShow] = React.useState(false)
+  const [postModalShow, setPostModalShow] = React.useState(false)
+
+  function logout() {
+    props.resetState()
+    Axios
+      .get('/auth/logout')
+      .catch(err => console.log(err))
+  }
+
   return (
     <nav>
       <PostModal
@@ -24,6 +34,8 @@ export default function Navbar(props) {
       <LoginModal
         show={loginModalShow}
         onHide={() => setLoginModalShow(false)}
+        loginModalShow={loginModalShow}
+        toggleModal={setLoginModalShow}
       />
       <RegisterModal
         show={registerModalShow}
@@ -56,22 +68,62 @@ export default function Navbar(props) {
               </div>
               <div className="user">
                 <img src="https://images.offerup.com/G_WanU6u5IpL1vVVfMajOkU48es=/100x100/smart/cb71/o63608076_48635.jpg" alt="" />
-                <div className="name">Chase</div>
+                <DropdownButton
+                  title="Chase"
+                  className="profile-dropdown"
+                  variant="none"
+                  alignRight
+                  id="dropdown-menu-align-right"
+                >
+                  <Dropdown.Item eventKey="1">Sell</Dropdown.Item>
+                  <Dropdown.Item eventKey="1">About</Dropdown.Item>
+                  <Dropdown.Item eventKey="1">My offers</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">My boards</Dropdown.Item>
+                  <Dropdown.Item eventKey="3">My profile</Dropdown.Item>
+                  <Dropdown.Item eventKey="4">My account</Dropdown.Item>
+                  <Dropdown.Item onClick={logout} eventKey="5">Log out</Dropdown.Item>
+                </DropdownButton>
               </div>
             </div>
           }
         </div>
-        <DropdownButton 
-          className="menu-icon"
-          variant="none"
-          alignRight
-          id="dropdown-menu-align-right"
-        >
-          <Dropdown.Item eventKey="2">Cars & Trucks</Dropdown.Item>
-          <Dropdown.Item eventKey="1">Tools & Machinery</Dropdown.Item>
-          <Dropdown.Item eventKey="3">Sports & Outdoors</Dropdown.Item>
-        </DropdownButton>
+        {!props.isLoggedIn
+          ? <DropdownButton
+            title=""
+            className="menu-icon"
+            variant="none"
+            alignRight
+            id="dropdown-menu-align-right"
+          >
+            <Dropdown.Item eventKey="1">Login</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Sign up</Dropdown.Item>
+            <Dropdown.Item eventKey="3">About</Dropdown.Item>
+          </DropdownButton>
+          : <DropdownButton
+            title=""
+            className="menu-icon"
+            variant="none"
+            alignRight
+            id="dropdown-menu-align-right"
+          >
+            <Dropdown.Item eventKey="1">Sell</Dropdown.Item>
+            <Dropdown.Item eventKey="1">About</Dropdown.Item>
+            <Dropdown.Item eventKey="1">My offers</Dropdown.Item>
+            <Dropdown.Item eventKey="2">My boards</Dropdown.Item>
+            <Dropdown.Item eventKey="3">My profile</Dropdown.Item>
+            <Dropdown.Item eventKey="4">My account</Dropdown.Item>
+            <Dropdown.Item onClick={logout} eventKey="5">Log out</Dropdown.Item>
+          </DropdownButton>
+        }
       </div>
     </nav>
   );
 }
+
+function mapStateToProps(reduxState) {
+  return {
+    isLoggedIn: reduxState.userReducer.login
+  }
+}
+
+export default connect(mapStateToProps, { resetState })(Navbar)
