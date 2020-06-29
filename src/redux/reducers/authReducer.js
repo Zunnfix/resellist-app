26a86e1e0396c2d594e74ece967ef5902e127c0f
cwshields <cwshields2@gmail.com>
2020-06-29 15:39:58 -0500

    changed to BrowserRouter, added comments

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -3,9 +3,11 @@ const initialState = {
   password: ''
 }
 
+// Variables set to avoid possible misspellings of string
 const SET_USERNAME = 'SET_USERNAME'
 const SET_PASSWORD = 'SET_PASSWORD'
 
+// Runs when state values are submitted
 export function setUsername(value) {
   return {
     type: SET_USERNAME,
@@ -19,6 +21,7 @@ export function setPassword(value) {
   }
 }
 
+// Changes state with each keystroke
 export default function reducer(state=initialState, action) {
   const { type, payload } = action
   switch(type) {
